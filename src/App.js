71a340c1d4cdb5e824210bbf2bda9c32ad0473ae
Rebekah Cruz <rebekah.cruz702@gmail.com>
2022-02-13 11:35:26 -0600

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Hello World</h1>
-      <footer>
-        This project was coded by Rebekah Cruz and is
-      < a href="https://github.com/bexter89/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github</a>
-      </footer>
+        <footer>
+          This project was coded by Rebekah Cruz and is
+        < a href="https://github.com/bexter89/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github</a>
+        </footer>
+      </div>
     </div>
   );
 }
