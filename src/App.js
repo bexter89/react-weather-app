4bec9c2f5ff43fcaae6fcfd6b84bb58aa247d1bc
Rebekah Cruz <rebekah.cruz702@gmail.com>
2022-02-13 11:29:49 -0600

    add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <footer>
+        This project was coded by Rebekah Cruz and is
+      < a href="https://github.com/bexter89/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
